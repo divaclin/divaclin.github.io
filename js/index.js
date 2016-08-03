4d9680fa665a920671f6ac3f5f1c5a218f3e0aec
divac lin <divac.d.zoro@gmail.com>
2016-08-04 00:13:52 +0800

    8/4 flex heder nav ul

--- js/index.js
+++ js/index.js
@@ -12,6 +12,6 @@ firebase.database().ref('/nav').once('value').then(function(snapshot) {
   var navList = snapshot.val();
   var navListTarget = document.getElementById('navList');   
     for(var key in navList){
-        navListTarget.innerHTML+='<li><a href="#'+key+'">'+key+'</a></li>';
+        navListTarget.innerHTML+='<li class="liBox"><a href="#'+key+'">'+key+'</a></li>';
     }
 });
